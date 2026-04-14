266df42729e0fa2b90a056d425518e841b36e7cc
Beer van der Drift <beerdrift@gmail.com>
2026-04-14 21:44:49 +0200

    feat: switch to TS project

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from hellm-world!")
-
-
-if __name__ == "__main__":
-    main()
--- token_embeddings.py
+++
@@ -1,5 +0,0 @@
-tokens = ["hello", "world", "my", "name", "is", "beer"]
-
-
-
-def get_token_vector(token: str):
