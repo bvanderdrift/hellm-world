f5dbd65944c96addb36bdaf01088cf8dce5afbad
Beer van der Drift <beerdrift@gmail.com>
2026-04-15 14:20:57 +0200

    test: add tokenizer tests

---
+++ tokenizer.test.ts
@@ -0,0 +1,23 @@
+import { describe, expect, it } from "vitest";
+import { tokenize } from "./tokenizer.ts";
+
+describe("tokenize", () => {
+  it("tokenizes a known sentence into vocabulary tokens", () => {
+    expect(tokenize("hello world my name is beer")).toEqual([
+      "hello",
+      "world",
+      "my",
+      "name",
+      "is",
+      "beer",
+    ]);
+  });
+
+  it("handles repeated tokens", () => {
+    expect(tokenize("hello hello world")).toEqual(["hello", "hello", "world"]);
+  });
+
+  it("throws when the input contains an unknown token", () => {
+    expect(() => tokenize("hello mars")).toThrow("Unable to tokenize mars");
+  });
+});
