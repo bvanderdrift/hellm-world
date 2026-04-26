344e379f540b07df888846cb198a8b3f407202f2
Beer van der Drift <beerdrift@gmail.com>
2026-04-26 16:45:31 +0200

    feat: allow model identifier in llm command

--- index.ts
+++ index.ts
@@ -5,19 +5,20 @@ import { doTrainingLoopAndStoreCheckpoint } from "./training/training.ts";
 program
   .name("llm")
   .command("run")
+  .argument("<model>", "model to run")
   .argument("<input...>", "raw input to complete")
-  .action((inputSeperated: string[]) => {
-    console.log(runLlm(inputSeperated.join(" "), "toy_model"));
+  .action((modelName: string, inputSeperated: string[]) => {
+    console.log(runLlm(inputSeperated.join(" "), modelName));
   });
 
 program
   .name("train")
   .command("train")
+  .argument("<model>", "model to run")
   .option(
     "-s, --steps <steps>",
     "Amount of steps before storing another checkpoint",
   )
-  .argument("<model>", "model to run")
   .action((modelName: string, opts: { steps: number }) => {
     doTrainingLoopAndStoreCheckpoint(modelName, opts.steps);
   });
