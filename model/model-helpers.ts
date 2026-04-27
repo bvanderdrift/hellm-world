48147ddfa768119e1267ffe75e1362313ad06942
Beer van der Drift <beerdrift@gmail.com>
2026-04-27 10:19:14 +0200

    refactor: move model validation into seperate file

--- model/model-helpers.ts
+++ model/model-helpers.ts
@@ -1,97 +1,10 @@
-import { END_OF_SEQUENCE_TOKEN } from "../shared/const.ts";
-import { divideToWhole } from "../shared/math.ts";
-import {
-  operateOnMatrices,
-  operateOnVectors,
-  validateSize,
-} from "../shared/matrices.ts";
+import { operateOnMatrices, operateOnVectors } from "../shared/matrices.ts";
+import { validateSameWeightShape } from "./model-validation.ts";
 import type { TransformerWeights, Model } from "./types.ts";
 
 export const extractHiddenDimensionSize = (model: Model) => {
   const embeddingsArray = Object.values(model.embeddings);
   return embeddingsArray[0]!.length;
-};
-
-export const validateModel = (model: Model) => {
-  if (model.vocabulary.length === 0) {
-    throw new Error("Provided vocabulary cannot be empty");
-  }
-
-  const hiddenDimensionsSize = extractHiddenDimensionSize(model);
-
-  if (model.headsCount <= 0) {
-    throw new Error("headsCount must be a positive integer");
-  }
-
-  divideToWhole(hiddenDimensionsSize, model.headsCount);
-
-  const tokensDeduped = new Set(model.vocabulary);
-
-  const duplicateCount = model.vocabulary.length - tokensDeduped.size;
-
-  if (duplicateCount > 0) {
-    throw new Error(`Provided weights have ${duplicateCount} duplicate tokens`);
-  }
-
-  if (!tokensDeduped.has(END_OF_SEQUENCE_TOKEN)) {
-    throw new Error(
-      `Model embeddings are missing special end-of-sequence token "${END_OF_SEQUENCE_TOKEN}"`,
-    );
-  }
-
-  validateSize(model.embeddings, model.vocabulary.length, hiddenDimensionsSize);
-  validateSize(
-    model.unembeddings,
-    hiddenDimensionsSize,
-    model.vocabulary.length,
-  );
-
-  for (const transformer of model.transformers) {
-    validateSize(
-      transformer.attention.Q,
-      hiddenDimensionsSize,
-      hiddenDimensionsSize,
-    );
-    validateSize(
-      transformer.attention.K,
-      hiddenDimensionsSize,
-      hiddenDimensionsSize,
-    );
-    validateSize(
-      transformer.attention.V,
-      hiddenDimensionsSize,
-      hiddenDimensionsSize,
-    );
-
-    validateSize(
-      transformer.attention.out,
-      hiddenDimensionsSize,
-      hiddenDimensionsSize,
-    );
-
-    // MLP validation
-    validateSize(
-      transformer.multilayerPerceptron.wUp.weightsMatrix,
-      hiddenDimensionsSize,
-      hiddenDimensionsSize * model.mlpMultiple,
-    );
-    validateSize(
-      [transformer.multilayerPerceptron.wUp.biasVector],
-      1,
-      hiddenDimensionsSize * model.mlpMultiple,
-    );
-
-    validateSize(
-      transformer.multilayerPerceptron.wDown.weightsMatrix,
-      hiddenDimensionsSize * model.mlpMultiple,
-      hiddenDimensionsSize,
-    );
-    validateSize(
-      [transformer.multilayerPerceptron.wDown.biasVector],
-      1,
-      hiddenDimensionsSize,
-    );
-  }
 };
 
 export const findTokenIndex = (vocabulary: string[], token: string) => {
@@ -102,34 +15,6 @@ export const findTokenIndex = (vocabulary: string[], token: string) => {
   }
 
   return tokenIndex;
-};
-
-export const validateSameWeightShape = (weights1: Model, weights2: Model) => {
-  const allTokensExactMatch = weights1.vocabulary.every(
-    (token1, tokenIndex) => token1 === weights2.vocabulary[tokenIndex],
-  );
-
-  if (!allTokensExactMatch) {
-    throw new Error(`Vocabularies between weights don't match`);
-  }
-
-  validateSize(
-    weights1.embeddings,
-    weights2.embeddings.length,
-    weights2.embeddings[0]!.length,
-  );
-
-  if (weights1.transformers.length !== weights2.transformers.length) {
-    throw new Error(
-      `Weights1 has different transformers count ${weights1.transformers.length} than Weights2 (${weights2.transformers.length})`,
-    );
-  }
-
-  if (weights1.headsCount !== weights2.headsCount) {
-    throw new Error(
-      `Weights1 has different head count ${weights1.headsCount} than Weights2 (${weights2.headsCount})`,
-    );
-  }
 };
 
 export const operateSingleWeights = (
@@ -220,3 +105,7 @@ export const operateCombinedWeights = (
 
 export const makeZeroVersion = (weights: Model) =>
   operateSingleWeights(weights, () => 0);
+
+export const getParameterCount = (model: Model) => {
+  const embeddingsParameterCount = mode
+}
---
+++ model/model-validation.ts
@@ -0,0 +1,115 @@
+import { END_OF_SEQUENCE_TOKEN } from "../shared/const.ts";
+import { divideToWhole } from "../shared/math.ts";
+import { validateSize } from "../shared/matrices.ts";
+import { extractHiddenDimensionSize } from "./model-helpers.ts";
+import type { Model } from "./types.ts";
+
+export const validateModel = (model: Model) => {
+  if (model.vocabulary.length === 0) {
+    throw new Error("Provided vocabulary cannot be empty");
+  }
+
+  const hiddenDimensionsSize = extractHiddenDimensionSize(model);
+
+  if (model.headsCount <= 0) {
+    throw new Error("headsCount must be a positive integer");
+  }
+
+  divideToWhole(hiddenDimensionsSize, model.headsCount);
+
+  const tokensDeduped = new Set(model.vocabulary);
+
+  const duplicateCount = model.vocabulary.length - tokensDeduped.size;
+
+  if (duplicateCount > 0) {
+    throw new Error(`Provided weights have ${duplicateCount} duplicate tokens`);
+  }
+
+  if (!tokensDeduped.has(END_OF_SEQUENCE_TOKEN)) {
+    throw new Error(
+      `Model embeddings are missing special end-of-sequence token "${END_OF_SEQUENCE_TOKEN}"`,
+    );
+  }
+
+  validateSize(model.embeddings, model.vocabulary.length, hiddenDimensionsSize);
+  validateSize(
+    model.unembeddings,
+    hiddenDimensionsSize,
+    model.vocabulary.length,
+  );
+
+  for (const transformer of model.transformers) {
+    validateSize(
+      transformer.attention.Q,
+      hiddenDimensionsSize,
+      hiddenDimensionsSize,
+    );
+    validateSize(
+      transformer.attention.K,
+      hiddenDimensionsSize,
+      hiddenDimensionsSize,
+    );
+    validateSize(
+      transformer.attention.V,
+      hiddenDimensionsSize,
+      hiddenDimensionsSize,
+    );
+
+    validateSize(
+      transformer.attention.out,
+      hiddenDimensionsSize,
+      hiddenDimensionsSize,
+    );
+
+    // MLP validation
+    validateSize(
+      transformer.multilayerPerceptron.wUp.weightsMatrix,
+      hiddenDimensionsSize,
+      hiddenDimensionsSize * model.mlpMultiple,
+    );
+    validateSize(
+      [transformer.multilayerPerceptron.wUp.biasVector],
+      1,
+      hiddenDimensionsSize * model.mlpMultiple,
+    );
+
+    validateSize(
+      transformer.multilayerPerceptron.wDown.weightsMatrix,
+      hiddenDimensionsSize * model.mlpMultiple,
+      hiddenDimensionsSize,
+    );
+    validateSize(
+      [transformer.multilayerPerceptron.wDown.biasVector],
+      1,
+      hiddenDimensionsSize,
+    );
+  }
+};
+
+export const validateSameWeightShape = (weights1: Model, weights2: Model) => {
+  const allTokensExactMatch = weights1.vocabulary.every(
+    (token1, tokenIndex) => token1 === weights2.vocabulary[tokenIndex],
+  );
+
+  if (!allTokensExactMatch) {
+    throw new Error(`Vocabularies between weights don't match`);
+  }
+
+  validateSize(
+    weights1.embeddings,
+    weights2.embeddings.length,
+    weights2.embeddings[0]!.length,
+  );
+
+  if (weights1.transformers.length !== weights2.transformers.length) {
+    throw new Error(
+      `Weights1 has different transformers count ${weights1.transformers.length} than Weights2 (${weights2.transformers.length})`,
+    );
+  }
+
+  if (weights1.headsCount !== weights2.headsCount) {
+    throw new Error(
+      `Weights1 has different head count ${weights1.headsCount} than Weights2 (${weights2.headsCount})`,
+    );
+  }
+};
