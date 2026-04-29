7a3a34e219ca95be678d1f6be86cf59a4adecad1
Beer van der Drift <beerdrift@gmail.com>
2026-04-29 20:56:30 +0200

    feat: calculate backprop of unembeddings matrix

--- training/backprop.ts
+++ training/backprop.ts
@@ -8,20 +8,22 @@ import {
 import type {
   Model,
   MultilayerPerceptronWeights,
+  TransformerWeights,
 } from "../model/model-types.ts";
 import { makeZeroVersion } from "../model/model-helpers.ts";
 import { calculateLoss } from "./calculateLoss.ts";
 import type {
   Activations,
   MultilayerPerceptronActivations,
+  TransformerActivations,
 } from "../model/activations-types.ts";
-import { sum } from "../shared/math.ts";
+import { softmax, sum } from "../shared/math.ts";
 
 export const backprop = (
   inputTokens: string[],
-  correctOutputToken: string,
   weights: Model,
   activations: Activations,
+  correctTokenIndex: number,
 ): {
   loss: number;
   gradients: Model;
@@ -35,12 +37,109 @@ export const backprop = (
     );
   }
 
-  validateSize([outputLogits], 1, weights.vocabulary.length);
+  const outputProbabilities = softmax(outputLogits);
+
+  const unembeddingsInputActivationsGradients = probabilityOutputBackprop(
+    activations.unembeddingsOutputLogits,
+    outputProbabilities,
+    inputTokens.length,
+    correctTokenIndex,
+  );
+
+  const {
+    weightGradients: unembeddingWeightGradients,
+    activationGradients: unembeddingInputActivationGradients,
+  } = matrixBackprop(
+    weights.unembeddings,
+    activations.transformersToUnembeddings,
+    unembeddingsInputActivationsGradients,
+  );
 
   return {
-    loss: calculateLoss(outputLogits, correctOutputToken, weights.vocabulary),
+    loss: calculateLoss(outputLogits, correctTokenIndex, weights.vocabulary),
     gradients: makeZeroVersion(weights),
   };
+};
+
+export const probabilityOutputBackprop = (
+  unembeddingsOutputLogits: number[][],
+  outputProbabilitiesVector: number[],
+  contextLength: number,
+  correctTokenIndex: number,
+) =>
+  unembeddingsOutputLogits.map((outputVector, inputTokenIndex) => {
+    if (inputTokenIndex !== contextLength - 1) {
+      // We're not training on these, so no gradients
+      return new Array(outputVector.length).fill(0);
+    }
+
+    return outputVector.map((_, vocabIndex) => {
+      const isCorrectToken = vocabIndex === correctTokenIndex;
+      const actualProbability = outputProbabilitiesVector[vocabIndex]!;
+      const wantedProbability = isCorrectToken ? 1 : 0;
+
+      /**
+       * This seems to be too easy and not like it's a derivative
+       * But it is. This is dL/dz_i
+       *
+       * Check this out:
+       *
+       * L = CEL_i = -log(p_i)
+       * p_i = exp(z_i) / sum_j exp(z_j)
+       * So L = -log(exp(z_i) / sum_j exp(z_j)) = -z_i + log(sum_j exp(z_j))
+       * So d(-z_i)/dz_i = -1 and d(log(n))/dn = 1 / n and d(log(sum_j exp(z_j)))/dz_i = (1 / (sum_j exp(z_j)) * exp(z_i) = exp(z_i) / (sum_j exp(z_j) = p_i
+       * Wowow; so dL/dz_i = d(-z_i)/dz_i + that sum part = -1 + p_i = p_i - 1 for i = k and 0 + p_j for non-i values (since d(z_j)/dz_i = 0)
+       */
+      return actualProbability - wantedProbability;
+    });
+  });
+
+export const transformersBackprop = (
+  outputGradients: number[][],
+  weights: TransformerWeights[],
+  activations: TransformerActivations[],
+) => {
+  if (weights.length !== activations.length) {
+    throw new Error(
+      `Transformer weights count ${weights.length} does not equal transformers activations count ${activations.length}`,
+    );
+  }
+
+  // Spread b/c reverse mutates in-place
+  const reversedActivations = [...activations].reverse();
+
+  let lastOutputGradients = outputGradients;
+
+  for (let index = 0; index < reversedActivations.length; index++) {
+    const transformerActivations = reversedActivations[index]!;
+    const transformerWeights = weights[weights.length - index - 1]!;
+
+    const updateActivationsGradients: number[][] = [
+      /** TODO */
+    ];
+
+    const { inputActivationGradients, weightGradients } = backpropMlp(
+      transformerWeights.multilayerPerceptron,
+      transformerActivations.mlp,
+      updateActivationsGradients,
+    );
+
+    const preNormalizationInputGradients = backpropNormalize(
+      inputActivationGradients,
+      transformerActivations.mlp.normalizedInputToUpping,
+    );
+
+    // TODO: Attention Backprop
+  }
+};
+
+export const backpropNormalize = (
+  outputGradients: number[][],
+  inputActivations: number[][],
+): [][] => {
+  // TODO
+
+  return [];
 };
 
 export const backpropMlp = (
@@ -55,17 +154,13 @@ export const backpropMlp = (
   // dL/db = dL/dMLP * dMLP/db = outputGradients * 1
   const downBiasGradient = addVectorsInMatrix(outputGradients);
 
-  const downWeightsGradients = matrixBackprop(
+  const {
+    weightGradients: downWeightsGradients,
+    activationGradients: downInputActivationsGradients,
+  } = matrixBackprop(
     weights.wDown.weightsMatrix,
     activations.nonLinearToDowning,
     outputGradients,
-  );
-
-  // dL/da = dl/dMLP * dMLP/da = outputGradients * wDownT.
-  // Simply because of a function `a * x + b`, if we take the derivative for `a` we simply have `x` which in this case is `w_ij` so we just transpose the weight matrix
-  const downInputActivationsGradients = multiplyMatrices(
-    outputGradients,
-    transpose(weights.wDown.weightsMatrix),
   );
 
   const upOutputGradients = reluBackprop(
@@ -75,15 +170,13 @@ export const backpropMlp = (
 
   const upBiasGradient = addVectorsInMatrix(upOutputGradients);
 
-  const upWeightsGradients = matrixBackprop(
+  const {
+    weightGradients: upWeightsGradients,
+    activationGradients: inputActivationGradients,
+  } = matrixBackprop(
     weights.wUp.weightsMatrix,
     activations.normalizedInputToUpping,
     upOutputGradients,
-  );
-
-  const inputActivationGradients = multiplyMatrices(
-    upOutputGradients,
-    transpose(weights.wUp.weightsMatrix),
   );
 
   return {
@@ -108,16 +201,29 @@ export const matrixBackprop = (
 ) => {
   const inputsByDimension = transpose(inputActivations);
 
-  return weights.map((incomingDimensionVector, incomingDimension) =>
-    incomingDimensionVector.map((_, outgoingDimension) => {
-      return sum(
-        inputsByDimension[incomingDimension]!.map(
-          (activation, tokenIndex) =>
-            activation * outputGradients[tokenIndex]![outgoingDimension]!,
-        ),
-      );
-    }),
+  const weightGradients = weights.map(
+    (incomingDimensionVector, incomingDimension) =>
+      incomingDimensionVector.map((_, outgoingDimension) => {
+        return sum(
+          inputsByDimension[incomingDimension]!.map(
+            (activation, tokenIndex) =>
+              activation * outputGradients[tokenIndex]![outgoingDimension]!,
+          ),
+        );
+      }),
   );
+
+  // dL/da = dl/dMLP * dMLP/da = outputGradients * wDownT.
+  // Simply because of a function `a * x + b`, if we take the derivative for `a` we simply have `x` which in this case is `w_ij` so we just transpose the weight matrix
+  const activationGradients = multiplyMatrices(
+    outputGradients,
+    transpose(weights),
+  );
+
+  return {
+    weightGradients,
+    activationGradients,
+  };
 };
 
 export const reluBackprop = (
--- training/calculateLoss.ts
+++ training/calculateLoss.ts
@@ -4,28 +4,22 @@ import { validateSize } from "../shared/matrices.ts";
 /**
  * Problem:
  * Normal Cross-entropy loss function is -Math.log(p_k)
- * p_k is calculated from logits. 
+ * p_k is calculated from logits.
  * A really small logit vs the others is valid.
  * But, Math.exp(small_logit) can be 0, very quickly. For example Math.exp(-1000) = 0 not e^-1000
  * No problem is probability space, but problematic in the loss function
  * Since we do want to calculate the gradient if the logit of the correct output token is small like -1000.
  * But if we just use probabilities; the Math.exp problem would've turned it into 0, and we would have calculated cost as Infinity
- * 
+ *
  * Solution:
  * We do softmax but without the normalizing (so not make the vector sum to 1)
  * This way the `correctTokenLogit` never has to be exponentiated for the loss calculation and can be used directly
  */
 export const calculateLoss = (
   outputLogits: number[],
-  correctToken: string,
+  correctTokenIndex: number,
   vocabulary: string[],
 ) => {
-  const correctTokenIndex = vocabulary.indexOf(correctToken);
-
-  if (correctTokenIndex === -1) {
-    throw new Error(`Failed to find token ${correctToken} in vocabulary`);
-  }
-
   validateSize([outputLogits], 1, vocabulary.length);
 
   const { safeLogits, biggestLogit } =
