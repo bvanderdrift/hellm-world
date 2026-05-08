dc3a06af16aad67ff1b361e9e8358139981e549b
Beer van der Drift <beerdrift@gmail.com>
2026-05-08 09:36:58 +0200

    refactor: DRY up matrix backprop calculation logic

--- training/backprop/matrixBackprop.ts
+++ training/backprop/matrixBackprop.ts
@@ -1,4 +1,3 @@
-import { sum } from "../../shared/math.ts";
 import { transpose, multiplyMatrices } from "../../shared/matrices.ts";
 
 export const matrixBackprop = (
@@ -8,17 +7,7 @@ export const matrixBackprop = (
 ) => {
   const inputsByDimension = transpose(inputActivations);
 
-  const weightGradients = weights.map(
-    (incomingDimensionVector, incomingDimension) =>
-      incomingDimensionVector.map((_, outgoingDimension) => {
-        return sum(
-          inputsByDimension[incomingDimension]!.map(
-            (activation, tokenIndex) =>
-              activation * outputGradients[tokenIndex]![outgoingDimension]!,
-          ),
-        );
-      }),
-  );
+  const weightGradients = multiplyMatrices(inputsByDimension, outputGradients);
 
   // dL/da = dl/dMLP * dMLP/da = outputGradients * wDownT.
   // Simply because of a function `a * x + b`, if we take the derivative for `a` we simply have `x` which in this case is `w_ij` so we just transpose the weight matrix
