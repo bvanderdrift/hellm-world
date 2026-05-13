02649c2784249da607b049555b5b27248ec5bf07
Beer van der Drift <beerdrift@gmail.com>
2026-05-13 20:53:48 +0200

    chore: add non-working gpu softmax placeholders

--- shared/math-gpu.ts
+++ shared/math-gpu.ts
@@ -22,3 +22,45 @@ export const reluOnGpu = (matrix: MatrixBuffer) => {
     .with(params)
     .dispatchThreads(matrix.vectors * matrix.dimensions);
 };
+
+const sumOnGPU = (values: number[]) => {
+  // "use gpu";
+
+  let sum = 0;
+
+  for (let index = 0; index < values.length; index++) {
+    sum += values[index]!;
+  }
+
+  return sum;
+};
+
+const meanOnGPU = (values: number[]) => {
+  // "use gpu";
+  return sumOnGPU(values) / values.length;
+};
+
+const safeSumExponatedLogitsOnGPU = (logits: number[]) => {
+  // "use gpu";
+  const biggestLogit = Math.max(...logits);
+  // To prevent overflows. Logits are still related the same since they all subtract the same value
+  const safeLogits = logits.map((logit) => logit - biggestLogit);
+  const exponatedLogits = safeLogits.map((l) => Math.exp(l));
+
+  return {
+    safeLogits,
+    exponatedLogits,
+    summed: sumOnGPU(exponatedLogits),
+    biggestLogit,
+  };
+};
+
+/**
+ * s_i = e^l_i / sum(e^l_j)
+ */
+export const softmaxOnGpu = (logits: number[]) => {
+  // "use gpu";
+  const { safeLogits, summed } = safeSumExponatedLogitsOnGPU(logits);
+
+  return safeLogits.map((logit) => Math.exp(logit) / summed);
+};
