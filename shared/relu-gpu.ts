47d36d2b81fe572ad1c75836d5b706cff4255d53
Beer van der Drift <beerdrift@gmail.com>
2026-06-13 12:32:51 +0200

    perf: move relu kernel creation outside of caller

--- shared/relu-gpu.ts
+++ shared/relu-gpu.ts
@@ -1,24 +1,23 @@
 import { gpuContext } from "./gpu-context.ts";
 import { singleMatrixParamsLayout, type MatrixBuffer } from "./matrices-gpu.ts";
 
+const reluKernel = gpuContext.createGuardedComputePipeline((i: number) => {
+  "use gpu";
+  const m = singleMatrixParamsLayout.$.m;
+
+  const currentValue = m.values[i]!;
+
+  if (currentValue > 0) {
+    m.values[i]! = currentValue;
+  } else {
+    m.values[i]! = 0;
+  }
+});
+
 export const reluOnGpu = (matrix: MatrixBuffer) => {
   const params = gpuContext.createBindGroup(singleMatrixParamsLayout, {
     m: matrix.buffer,
   });
 
-  gpuContext
-    .createGuardedComputePipeline((i: number) => {
-      "use gpu";
-      const m = singleMatrixParamsLayout.$.m;
-
-      const currentValue = m.values[i]!;
-
-      if (currentValue > 0) {
-        m.values[i]! = currentValue;
-      } else {
-        m.values[i]! = 0;
-      }
-    })
-    .with(params)
-    .dispatchThreads(matrix.vectors * matrix.dimensions);
+  reluKernel.with(params).dispatchThreads(matrix.vectors * matrix.dimensions);
 };
