6c6ca19a1c468af2534b9131a3379349526b18b7
Beer van der Drift <beerdrift@gmail.com>
2026-06-15 23:11:16 +0200

    fix: allow relu to dispatch on much bigger matrices

--- shared/relu-gpu.ts
+++ shared/relu-gpu.ts
@@ -1,9 +1,27 @@
+import tgpu from "typegpu";
 import { gpuContext } from "./gpu-context.ts";
 import { singleMatrixParamsLayout, type MatrixBuffer } from "./matrices-gpu.ts";
+import { builtin } from "typegpu/data";
 
-const reluKernel = gpuContext.createGuardedComputePipeline((i: number) => {
+const reluKernel = tgpu.computeFn({
+  in: {
+    localId: builtin.localInvocationId,
+    numWorkgroups: builtin.numWorkgroups,
+  },
+  workgroupSize: [16, 16],
+})((input) => {
   "use gpu";
+
+  const i1 = input.localId.x;
+  const i2 = input.localId.y;
+
+  const i = i1 * input.numWorkgroups.x + i2;
+
   const m = singleMatrixParamsLayout.$.m;
+
+  if (i >= m.vectors * m.dimensions) {
+    return;
+  }
 
   const currentValue = m.values[i]!;
 
@@ -14,10 +32,19 @@ const reluKernel = gpuContext.createGuardedComputePipeline((i: number) => {
   }
 });
 
+const reluPipeline = gpuContext.createComputePipeline({
+  compute: reluKernel,
+});
+
 export const reluOnGpu = (matrix: MatrixBuffer) => {
   const params = gpuContext.createBindGroup(singleMatrixParamsLayout, {
     m: matrix.buffer,
   });
 
-  reluKernel.with(params).dispatchThreads(matrix.vectors * matrix.dimensions);
+  reluPipeline
+    .with(params)
+    .dispatchWorkgroups(
+      Math.ceil(matrix.vectors / 16),
+      Math.ceil(matrix.dimensions / 16),
+    );
 };
